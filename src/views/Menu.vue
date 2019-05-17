30aed646c0aff0d1f8725cfd58090cd875f62cb0
Elena Hernandez <hrndzelena@gmail.com>
2019-05-16 22:46:24 -0500

    add orders on firebase & solve problems
    Solve firebase problems
    Add main order components

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -4,51 +4,48 @@
 
   <div class="menu">
     <div class="main">
-        <label for="beef_burger"><input type="checkbox" id="beef_burger" v-bind:value="true" v-model="vue.beef"><img class="button_img" src="../assets/beef_burger.png" alt="beef_burger"></label>
-        <label for="chicken_burger"><input type="checkbox" id="chicken_burger" v-bind:value="true" v-model="vue.chicken"><img class="button_img" src="../assets/chicken_burger.png" alt="chicken_burger"></label>
-        <label for="veggie_burger"><input type="checkbox" id="veggie_burger" v-bind:value="true" v-model="vue.veggie"><img class="button_img" src="../assets/veggie_burger.png" alt="veggie_burger"></label>
-        <label for="sandwich"><input type="checkbox" id="sandwich" v-bind:value="false" v-model="vue.sandwich"><img class="button_img" src="../assets/sandwich.png" alt="sandwich"></label>
+        <label for="beef_burger"><input type="checkbox" v-on:click="creteAnOrder('beef')" id="beef_burger" v-bind:value="true"><img class="button_img" src="../assets/beef_burger.png" alt="beef_burger"></label>
+        <label for="chicken_burger"><input type="checkbox" v-on:click="creteAnOrder('chiken')" id="chicken_burger" v-bind:value="true"><img class="button_img" src="../assets/chicken_burger.png" alt="chicken_burger"></label>
+        <label for="veggie_burger"><input type="checkbox" v-on:click="creteAnOrder('veggie')" id="veggie_burger" v-bind:value="true"><img class="button_img" src="../assets/veggie_burger.png" alt="veggie_burger"></label>
+        <label for="sandwich"><input type="checkbox" v-bind:value="false" id="sandwich" v-model="vue.sandwich"><img class="button_img" src="../assets/sandwich.png" alt="sandwich"></label>
     </div>
 
     <div class="complemets">
       <div class="fries">
-        <button type="button" name="button"><img class="button_img" src="../assets/french_fries.png" alt="french_friesr"></button>
-        <button type="button" name="button"><img class="button_img" src="../assets/onion_rings.png" alt="onion_rings"></button>
+        <label for="french_fires"><input type="checkbox" v-on:click="selectComplement" id="french_fires"><img class="button_img" src="../assets/french_fries.png" alt="french_friesr"></label>
+        <label for="onion_rings"><input type="checkbox" v-on:click="selectComplement" id="onion_rings"><img class="button_img" src="../assets/onion_rings.png" alt="onion_rings"></label>
       </div>
       <div class="menu-complements">
-        <div v-if="vue.beef | vue.chicken | vue.veggie"  class="tamaño">
-        <label for=""><input type="checkbox" v-model="vue.size" v-bind:value="true" name="button"><h3>SENCILLA</h3></label>
-        <label for=""><input type="checkbox" v-model="vue.size" v-bind:value="true" name="button"><h3>DOBLE</h3></label>  
+        <div v-if="vue.protein"  class="tamaño">
+        <label for="sencilla"><input type="checkbox" id="sencilla"v-model="vue.size" v-on:click="selectBreadSize(false)" v-bind:value="true" name="button"><h3>SENCILLA</h3></label>
+        <label for="doble"><input type="checkbox" id="doble" v-model="vue.size" v-on:click="selectBreadSize(true)" v-bind:value="true" name="button"><h3>DOBLE</h3></label>
         </div>
         <div v-if="vue.sandwich | vue.size" class="vegetables">
-          <button type="button" name="button"><img class="button_img" src="../assets/onion.png" alt="soda"></button>
-          <button type="button" name="button"><img class="button_img" src="../assets/tomatoe.png" alt="wather"></button>
-          <button type="button" name="button"><img class="button_img" src="../assets/lettuce.png" alt="soda"></button>
-          <button type="button" name="button"><img class="button_img" src="../assets/pepper.png" alt="wather"></button>
-          <button type="button" name="button"><h3>NINGUNO</h3></button>
+          <label for="onion"><input type="checkbox" v-on:click="selectVegetables" id="onion"><img class="button_img" src="../assets/onion.png" alt="soda"></label>
+          <label for="tomatoe"><input type="checkbox" v-on:click="selectVegetables" id="tomatoe"><img class="button_img" src="../assets/tomatoe.png" alt="wather"></label>
+          <label for="lettuce"><input type="checkbox" v-on:click="selectVegetables" id="lettuce"><img class="button_img" src="../assets/lettuce.png" alt="soda"></label>
+          <label for="pepper"><input type="checkbox" v-on:click="selectVegetables" id="pepper"><img class="button_img" src="../assets/pepper.png" alt="wather"></label>
+          <label for="any"><input type="checkbox" v-on:click="selectVegetables" id="any"><h3>NINGUNO</h3></label>
         </div>
     </div>
 
   </div>
   <div class="beverage">
-    <button type="button" name="button"><img class="button_img" src="../assets/juce.png" alt="juce"></button>
-    <button type="button" name="button"><img class="button_img" src="../assets/coffe.png" alt="coffe"></button>
-    <button type="button" name="button"><img class="button_img" src="../assets/soda.png" alt="soda"></button>
-    <button type="button" name="button"><img class="button_img" src="../assets/wather.png" alt="wather"></button>
+    <label for="juce"><input type="checkbox" v-on:click="selectBeverage" id="juce"><img class="button_img" src="../assets/juce.png" alt="juce"></label>
+    <label for="coffe"><input type="checkbox" v-on:click="selectBeverage" id="coffe"><img class="button_img" src="../assets/coffe.png" alt="coffe"></label>
+    <label for="soda"><input type="checkbox" v-on:click="selectBeverage" id="soda"><img class="button_img" src="../assets/soda.png" alt="soda"></label>
+    <label for="wather"><input type="checkbox" v-on:click="selectBeverage" id="wather"><img class="button_img" src="../assets/wather.png" alt="wather"></label>
   </div>
 
   </div>
 
   <div class="tiket">
     <img class="tiket-libreta" src="../assets/libreta.png" alt="tiket">
-    <form @submit="addClient(client)">
-      <input v-model="client" placeholder="Cliente">
-      <p style="white-space: pre-line;">{{ complemet }}</p>
+    <form v-on:submit.prevent="addClient(client, orders)">
+      <input class="nombre" v-model="client" placeholder="Cliente">
         <br>
-      <p>{{vue}}</p>
-      <p>{{vegetables}}</p>
+      <p>orders are : {{orders}}</p>
         <br>
-      <p>{{beverage}}</p>
       <button type="submit" class="button">Agregar nuevo cliente</button>
     </form>
   </div>
@@ -63,22 +60,18 @@ export default {
   name: 'Menu',
   data () {
     return {
+      currentOrder: 0,
+      beef: 0,
+      chicken: 0,
+      veggie:0,
+      sandwich: 0,
+      orders: [],
+      isOrderFinish: true,
       vue:{
-      beef: null,
-      chicken:null,
-      veggie:null,
-      sandwich: null,
-      size:null,
-    },
-      client: '',
-      orders:[],
-      beverage: '',
-      complemet: '',
-      mainDish: {},
-      bread:'',
-      extras:[],
-      protein:'',
-      vegetables:[],
+        size:null,
+        protein: false
+      },
+      client: ''
     }
   },
   firestore () {
@@ -87,15 +80,68 @@ export default {
     }
   },
   methods: {
-    addClient(client){
-      db.collection('orders').add({client})
-    }
+    addClient(client, orders){
+      db.collection('orders').add({client, orders})
+    },
+    incrementar (){
+      this.number++
+    },
+    creteAnOrder: function(proteinToSet){
+      if(!this.vue.protein){
+        let order = {
+          beverage: '',
+          complemet: '',
+          mainDish: {
+            bread:'',
+            protein:proteinToSet,
+            vegetables:[],
+            extras:[],
+          },
+
+        }
+        this.orders.push(order);
+        this.vue.protein = true;
+      }
+      else{
+        if(this.orders[this.currentOrder].mainDish.protein == proteinToSet){
+          this.orders.shift(this.orders[this.currentOrder]);
+          this.vue.protein = false;
+        }
+        else {
+          this.orders[this.currentOrder].mainDish.protein = proteinToSet
+        }
+      }
+    },
+    selectBreadSize: function(isJumbo){
+      let sizeOfBread = "jumbo";
+      if(!isJumbo){
+        sizeOfBread = "big"
+      }
+      this.orders[this.currentOrder].mainDish.bread = sizeOfBread;
+    },
+    selectBeverage: function(){},
+    selectComplement:function(){},
+    selectVegetables: function(){}
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+input{
+  display: none;
+}
+
+form{
+  z-index: 1;
+  margin-top: 3%;
+}
+
+input{
+  background-color: transparent;
+  border: transparent;
+}
 
 label {
   background-color: transparent;
@@ -108,13 +154,8 @@ label {
   align-items: center;
 }
 
-form{
-  z-index: 1;
-  margin-top: 3%;
+.nombre{
+  display: block;
 }
 
-input{
-  background-color: transparent;
-  border: transparent;
-}
 </style>
