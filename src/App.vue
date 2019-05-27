f2db53108bc69a0db1e47873c324dbce04f96f3f
ElenaHrndz <hrndzelena@gmail.com>
2019-05-27 00:39:09 -0500

    up date

--- src/App.vue
+++ src/App.vue
@@ -8,120 +8,82 @@
 export default {
   name: 'App'
 }
-//
-//
-// import { db } from './main'
-//
-// export default {
-//   name: 'App',
-//   data () {
-//     return {
-//       client: ''
-//     }
-//   },
-//   firestore () {
-//     return {
-//       locations: db.collection('orders')
-//     }
-//     },
-//   methods: {
-//     setClient(client){
-//       db.collection('orders').add({client})
-//     }
-//   }
-// }
-
 </script>
 
 <style>
+a{
+  text-decoration: none;
+}
 body {
   background-image: url("./assets/background.png");
   background-attachment: fixed;
   background-repeat-y: no-repeat;
   background-size: contain;
 }
-
 button {
-  background-color: transparent;
+  background-color: wheat;
   border:none;
   display: flex;
   justify-content: center;
-  align-items: center;
 }
-
 h2 {
   position: absolute;
   font-size: 4em;
   margin: 0;
 }
-
-
 h3 {
   font-size: 1.35em;
   margin: 0;
-  background-color: wheat;
+  /* background-color: wheat; */
 }
-
 input {
   display: flex;
   justify-content: center;
 }
-
 table {
   display: inline;
 }
-
-
 .button_img {
     height: inherit;
     width: inherit;
   }
-
   .complemets {
     display: inline;
   }
-
   .fries{
     display: flex;
     justify-content: center;
     margin-bottom: 5%;
   }
-
   .menu {
     display: flex;
     justify-content: space-between;
   }
-
   .menu-complements {
     display: inline;
   }
-
-  .tamaño {
+  .size {
     display: flex;
     align-self: flex-start;
     margin-bottom: 5%;
     justify-content: center;
   }
-
   .tiket {
     height: 100%;
     width: 100%;
     display: flex;
     justify-content: flex-end;
   }
-
   .tiket-libreta {
     margin: 2% -1% 0 0;
     height: 45%;
     position: absolute;
   }
-
   .vegetables {
     display: flex;
     justify-content: center;
     align-items: center;
   }
-
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
